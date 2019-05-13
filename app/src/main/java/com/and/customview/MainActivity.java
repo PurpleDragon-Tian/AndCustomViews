377dd352d36de4b1f6efc28bcd2a6a8f358e7e7d
tzl0588@163.com <tianzilong0505_>
2019-05-13 20:17:17 +0800

    1、增加了对ButterKnife注解框架的支持

--- app/src/main/java/com/and/customview/MainActivity.java
+++ app/src/main/java/com/and/customview/MainActivity.java
@@ -1,23 +1,32 @@
 package com.and.customview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.and.customview.activity.label.InputableLabelActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvInputableLabel)
+    TextView tvInputableLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page_activity);
-
+        ButterKnife.bind(this);
     }
 
-    public void InputableLabel(View view) {
+    @OnClick(R.id.tvInputableLabel )
+    void tvInputableLabel() {
         startActivity(new Intent(MainActivity.this, InputableLabelActivity.class));
     }
+
 }
